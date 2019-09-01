a1be8171a6da6b1058ecba551360a63b04045456
Alexander van Oostenrijk <alex.vanoostenrijk@gmail.com>
2019-09-01 18:40:55 +0200

    Publish script

---
+++ pub.bat
@@ -0,0 +1,9 @@
+rem Publish package to NPM, eliminating this /dist directory (publish only its contents)
+rem This is done by copying package.json and README.md to dist, then editing package.json
+rem to remove the /dist prefix.
+tsc
+copy package.json dist
+copy README.md dist
+node node_modules\replace-in-file\bin\cli.js /dist/g "" dist/package.json --isRegex
+npm version patch
+npm publish dist
