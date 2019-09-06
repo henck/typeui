e3e1ade505c8d5b47c43c94da546007514c55d43
Alexander van Oostenrijk <alex.vanoostenrijk@gmail.com>
2019-09-06 16:02:33 +0200

    Webpack emit svg

--- webpack.config.js
+++ webpack.config.js
@@ -4,19 +4,6 @@ const path = require('path'),
 module.exports = {
   mode: 'development',
   entry: './src/index.ts',
-  // Consider js, jsx, ts and tsx for bundling.
-  resolve: {
-    extensions: ['.js', '.jsx', '.ts', '.tsx']
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(ts|tsx)$/,
-        loader: 'ts-loader',
-        exclude: /node_modules/
-      }
-    ]
-  },
   plugins: [
     new SVGSpritemapPlugin('svg/**/*.svg', {
       sprite: {
