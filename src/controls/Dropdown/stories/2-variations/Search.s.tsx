c5cd4c0ee5e51c561dea3ca7ce06aa7ae3e26edd
Alexander van Oostenrijk <alex.vanoostenrijk@gmail.com>
2019-12-12 18:29:13 +0200

    Add Dropdown search story

---
+++ src/controls/Dropdown/stories/2-variations/Search.s.tsx
@@ -0,0 +1,27 @@
+import * as React from 'react';
+import { withInfoSettings, withStyledComponents, storiesOf, withInfo, withKnobs } from '../../../../StorybookBase.s';
+import { boolean } from '@storybook/addon-knobs';
+import { Dropdown } from '../../../Dropdown'
+
+storiesOf('Controls/Dropdown/Variations', module)
+  .addDecorator(withInfo({...withInfoSettings, propTables: false}))
+  .addDecorator(withStyledComponents)
+  .addDecorator(withKnobs)
+  .addParameters({
+    info: `
+    If an \`onSearch\` callback is provided, then the \`Dropdown\` will have a search box. When the user
+    types in the search box, the callback is called with the search string. There is a debounce or 350ms
+    in place so the search callback is not called too often. This gives your code an opportunity to provide
+    the \`Dropdown\` with new items based on the search string.
+    `
+  })
+  .addWithJSX(
+    'Search',
+  () => (
+  <div>
+    <Dropdown placeholder="Select person" onSearch={(q:string) => {}} label={(item:any) => item.name} data={[{id: 1, name: 'John', age: 26}, {id: 2, name:'Zachary', age:28}, {id: 3, name:'Deke', age:19}]}>
+      <Dropdown.Column>{(item) => <span>{item.name}</span>}</Dropdown.Column>
+      <Dropdown.Column align="right">{(item) => item.age}</Dropdown.Column>
+    </Dropdown>   
+  </div>
+  ));  
