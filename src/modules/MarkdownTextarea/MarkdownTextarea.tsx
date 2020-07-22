6c3bb8c023204c223f4697d814e091bd4e9a70f7
Alexander van Oostenrijk <alex.vanoostenrijk@gmail.com>
2020-07-22 10:15:57 +0200

    Add markdowntextarea translation options

--- src/modules/MarkdownTextarea/MarkdownTextarea.tsx
+++ src/modules/MarkdownTextarea/MarkdownTextarea.tsx
@@ -5,26 +5,42 @@ import { Segment } from '../../controls/Segment';
 import { Tabs } from '../../controls/Tabs';
 import { Markdown } from '../../formatters/Markdown';
 
-class MarkdownTextarea extends React.Component<ITextareaProps> {
+interface IProps {
+  classname?: string;
+  /**
+   * Optional translation for "Text" label
+   */
+  label_text?: React.ReactNode;
+  /**
+   * Optional translation for "Preview" label
+   */
+  label_preview?: React.ReactNode;
+  /**
+   * Optional translation for "is supported" label
+   */
+  label_markdown?: React.ReactNode;
+}
+
+class MarkdownTextarea extends React.Component<IProps & ITextareaProps> {
   render() {
-    let { className, ...otherProps } = this.props;
+    let { className, label_text, label_preview, label_markdown, ...textAreaProps } = this.props;
     return (
       <div className={className}>
         <Segment tight attached="top">
           <Tabs underlined>
-            <Tabs.Pane label="Text">
-              <Textarea monospaced fluid transparent {...otherProps}/>
+            <Tabs.Pane label={label_text ? label_text : 'Text'}>
+              <Textarea monospaced fluid transparent {...textAreaProps}/>
             </Tabs.Pane>
-            <Tabs.Pane label="Preview">
+            <Tabs.Pane label={label_preview ? label_preview : 'Preview'}>
               <Scrollable>
-                <Markdown source={otherProps.value}/>
+                <Markdown source={textAreaProps.value}/>
               </Scrollable>
             </Tabs.Pane>
           </Tabs>
         </Segment>
         <Segment tight secondary attached="bottom">
           <Options>
-             <a target="_blank" href="https://www.markdownguide.org/">Markdown</a> is supported
+             <a target="_blank" href="https://www.markdownguide.org/">Markdown</a> {label_markdown ? label_markdown : 'is supported'}
            </Options>
         </Segment>
       </div>
