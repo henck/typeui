0d7bf100cba22c67aea0bce126ef126a39dc0ca6
Alexander van Oostenrijk <alex.vanoostenrijk@gmail.com>
2021-04-20 08:37:55 +0200

    Remove padding under underlined tabs

--- src/controls/Tabs/Panes.tsx
+++ src/controls/Tabs/Panes.tsx
@@ -23,7 +23,7 @@ class PanesBase extends React.Component<IPanesProps, {}> {
 const Panes = styled(PanesBase)`
   height: 100%;
   flex: 1;
-  padding-top: 14px;
+  
   border-bottom-left-radius: ${p => p.theme.radius}px;
   border-bottom-right-radius: ${p => p.theme.radius}px;
 
