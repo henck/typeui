0637a4db92e60626d8f984c1ce07a73c68f07b77
Alexander van Oostenrijk <alex.vanoostenrijk@gmail.com>
2021-04-24 15:22:18 +0200

    Add active clock number scaling

--- src/controls/Input/Time/ClockNumber.tsx
+++ src/controls/Input/Time/ClockNumber.tsx
@@ -15,34 +15,49 @@ interface IProps {
  * A ClockNumber is a number on the face of the clock.
  */
 class ClockNumberBase extends React.Component<IProps> {
-  render() {
+  render = () => {
     let p = this.props;
-    return <div className={p.className}><ClockValue>{p.value}</ClockValue></div>
+    return (
+      <div className={p.className}>
+        <ClockValue>
+          {p.value}
+        </ClockValue>
+      </div>
+    );
   }
 }
 
+/**
+ * Actual value of clock number
+ */
+const ClockValue = styled('div')``
+
 const ClockNumber = styled(ClockNumberBase).attrs(p => ({
+  // Position clock number in the center, then at 40% offset of total clock size
+  // (this is in percentage)
   left: 50 + Math.cos((p.degrees-90) * Math.PI/180) * 40,
   top:  50 + Math.sin((p.degrees-90) * Math.PI/180) * 40,
   // The "3" and the "9" are slightly offset. Fix this:
   yFix: ((p.degrees >= 88 && p.degrees <= 92) || (p.degrees >= 268 && p.degrees <= 272)) ? 1 : 0
 }))`
   position: absolute;
-  left: calc(${p => p.left}% + ${p => p.yFix}px);
-  top:  calc(${p => p.top}% + ${p => p.yFix}px);
-  color: ${p => p.active ? 'white' : 'inherit'};
-  font-weight: ${p => p.active ? 500 : 'inherit'};
-`
-
-const ClockValue = styled('div')`
-  position: absolute;
-  left: -12.5px;
-  top: -0.6em;
-  width: 25px;
-  height: 1.2em;
-  line-height: 1.2em;
-  text-align: center;
-  user-select: none;
+  left:     calc(${p => p.left}% + ${p => p.yFix}px);
+  top:      calc(${p => p.top}% + ${p => p.yFix}px);
+  ${ClockValue} {
+    position:    absolute;
+    left:        -12.5px;
+    top:         -0.6em;
+    width:       25px;
+    height:      1.2em;
+    line-height: 1.2em;
+    text-align:  center;
+    user-select: none;
+    transform-origin: 50% 50%;
+    transition:  transform ${p => p.theme.transition.duration}s ease; 
+    color:       ${p => p.active ? 'white' : 'inherit'};
+    font-weight: ${p => p.active ? 500 : 'inherit'};
+    transform:   scaleX(${p => p.active ? 1.2 : 1}) scaleY(${p => p.active ? 1.2 : 1});
+  }
 `
 
 export { ClockNumber }
