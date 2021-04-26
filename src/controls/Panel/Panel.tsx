fbc1fc252de4968d269ea22f5dcbcdfee0b504cc
Alexander van Oostenrijk <alex.vanoostenrijk@gmail.com>
2021-04-26 15:54:59 +0200

    Fix displayName

--- src/controls/Panel/Panel.tsx
+++ src/controls/Panel/Panel.tsx
@@ -106,6 +106,6 @@ class Panel extends React.Component<IPanelProps, IPanelState> {
 (Panel.Header as any).displayName = "Panel.Header";
 (Panel.Content as any).displayName = "Panel.Content";
 (Panel.Footer as any).displayName = "Panel.Footer";
-(Panel.Icon as any).displayName = "Panel.IconPanel";
+(Panel.Icon as any).displayName = "Panel.Icon";
 
 export { Panel };
