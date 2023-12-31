ec5d6c125ae6663217d7136e01416c790b555caf
henck <alex.vanoostenrijk@gmail.com>
2023-12-31 15:11:21 +0000

    Fix Divider theme colors

--- src/controls/Divider/Divider.tsx
+++ src/controls/Divider/Divider.tsx
@@ -33,41 +33,39 @@ const DividerBase = (props: IDividerProps) =>
   <div className={props.className}><span>{props.children}</span></div>
 
 const DividerStyled = styled(DividerBase)`
-  position: relative; /* For internal <span> positioning */
+  position: relative; // For internal <span> positioning.
   box-sizing: border-box;
   font-weight: 500;
+  background-color: inherit; // Inherit here, so it can be passed on to the span.
 
-  /* A divider always clears content above it. */
+  // A divider always clears content above it.
   clear: both;
 
-  /* Margin:
-     A fitted divider has no vertical margin. 
-     A section divider has double vertical margin.*/
+  // Margin:
+  // A fitted divider has no vertical margin. 
+  // A section divider has double vertical margin.
   margin: 14px 0;
   ${p => p.fitted && css`margin: 0;`}
   ${p => p.narrow && css`margin: 7px 0;`}
   ${p => p.section && css`margin: 28px 0;`}
 
-  /* Border:
-     A hidden divider has no dividing line. */
-  border-top: solid 1px ${p => p.hidden ? p.theme.background : p.theme.normalColor};
+  // Border:
+  // A hidden divider has no dividing line.
+  border-top: solid 1px ${p => p.hidden ? 'transparent' : p.theme.normalColor};
 
-  /* A <span> is used to center the <Divider>'s content
-     vertically. */
+  // A <span> is used to center the <Divider>'s content vertically.
   & > span {
     position: absolute;
     left: 50%;
     top: 50%;
     transform: translateX(-50%) translateY(-50%);
     padding: 0 16px;
-    background: #fff;
+    background: inherit;
   }
 
-  /* Any elements contained in the <Divider> get their
-     vertical margins removed to vertically center 
-     them properly.
-     An example would be a <Header> contained in te
-     <Divider>. */
+  // Any elements contained in the <Divider> get their vertical margins removed 
+  // to vertically center them properly.  An example would be a <Header> 
+  // contained in the <Divider>.
   & span * {
     margin-top: 0;
     margin-bottom: 0;
