52ad682e9256aed86cb0fbec890da00f37b9f576
henck <alex.vanoostenrijk@gmail.com>
2024-01-10 12:25:29 +0000

    Uncontrolled now boxable

--- src/controls/Form/Uncontrolled.tsx
+++ src/controls/Form/Uncontrolled.tsx
@@ -5,26 +5,35 @@ import { css } from 'styled-components';
 // Other controls
 import { Hint } from './Hint';
 import { Label } from './Label';
+import { FieldWrapper } from './FieldWrapper';
 
 interface IProps {
   /** @ignore */
   className?: string;
   /** @ignore */
   children?: React.ReactNode;
-  /** Label to show, if any */
-  label?: string;
+  /** Label to show by field. Can be JSX. */
+  label?: React.ReactNode;
+  /**
+   * If true, field has a boxed appearance. This places a border around the 
+   * field, which is clickable and moves focus to the contained control.
+   */  
+  boxed?: boolean;
   /** 
    * Place label inline with field control. 
    * @default false
    */
   inline?: boolean;
-  /** Relative width of field. If not set, field will not flex to fill available width. */
+  /** 
+   * Relative width of field. If not set, field will not flex to fill 
+   * available width. 
+   */
   width?: number;  
   /** Hint to show */
   hint?: React.ReactNode;
 }
 
-const UncontrolledBase = (props: IProps) => {
+const Uncontrolled = (props: IProps) => {
 
   // Get field's label element, if any.
   const getLabel = (): React.ReactNode => {
@@ -34,21 +43,12 @@ const UncontrolledBase = (props: IProps) => {
   }  
 
   return (
-    <div className={props.className}>
+    <FieldWrapper boxed={props.boxed} width={props.width}>
       {getLabel()}
       {props.children}
       <Hint>{props.hint}</Hint>
-    </div>
+    </FieldWrapper>
   );
 }
 
-const UncontrolledStyled = styled(UncontrolledBase)`
-  padding-bottom: 8px;
-  /* Fields may provide their width in relative units to other fields. */
-  ${p => p.width && css `flex: ${p.width}`}
-`;
-
-const Uncontrolled = (props: IProps) =>
-  <UncontrolledStyled {...props}/>
-
-export { Uncontrolled, IProps }
+export { Uncontrolled }
