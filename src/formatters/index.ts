615d8f203a328eae8525a0fad9a14e8216339ca5
henck <alex.vanoostenrijk@gmail.com>
2024-02-04 14:01:41 +0000

    Disable global import of formatters, because that forces react-markdown dependency

--- src/formatters/index.ts
+++
@@ -1,7 +0,0 @@
-export { Datum } from './Datum';
-export { EUI } from './EUI'
-export { Filesize } from './Filesize'
-export { Latitude, Longitude } from './GIS'
-export { Human } from './Human'
-export { Markdown } from './Markdown'
-export { Number } from './Number'
